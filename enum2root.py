befa3b56ab97bc8984958ca9d3a2702ac838afad
Yusuf Hegazy <yusufthehegazy@gmail.com>
2020-10-11 23:36:47 +0200

    little edits

--- enum2root.py
+++ enum2root.py
@@ -21,34 +21,25 @@ def masscanFull(IP):
     logging.info("Scanning all ports with masscan")
     scan = subprocess.run("masscan -p1-65535,U:1-65535 {} --rate=1000 -oX -".format(IP), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
     out = scan.stdout.decode()
-    return out
+    return out #XML Output
 
 def nmapFullDetailed(IP,ports):
     logging.info("Starting a detailed nmap scan with masscan results")
-    scan = nmap.nmap_version_detection("10.10.41.38", args="-sC -T5 --min-rate=1000 --max-retries=2")
-    #scan = subprocess.run("nmap -p {} -sC -sV -T4 -oA ports.log {} ".format(ports, IP), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
-    out = scan.stdout.decode()
-    return out
+    scan = nmap.nmap_version_detection(str(IP), args="-sC -p {} -T5 --min-rate=1000 --max-retries=2".format(ports))
+    return scan #JSON Object
 
 def parseXML(XML):
     logging.info("parsing XML files")
-    #tree = ET.parse("{}".format(file))
     tree = ET.ElementTree(ET.fromstring(XML))
     root = tree.getroot()
     ports = [port.get('portid') for port in root.findall('.//port')]
     ports_string = ",".join(ports)
     return ports_string
 
-def basicEnum(IP, WORDLIST):
-    nmapBasicDetailed(IP)
-    nmapFullBasic(IP)
-    gobusterBasic(IP, WORDLIST)
-
-def detailedEnum(IP):
+def portEnum(IP):
     scan = masscanFull(IP)
     ports = parseXML(scan)
     final = nmapFullDetailed(IP,ports)
-    return final
-#basicEnum(sys.argv[1], sys.argv[2])
-#print(detailedEnum(sys.argv[1]))
+    return final #JSON Output with all the needed details
 
+print(portEnum(argv[1]))
